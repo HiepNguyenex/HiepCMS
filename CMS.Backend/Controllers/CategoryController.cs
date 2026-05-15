7b699fa272094496d348dc1c32b5376ec6655b6c
HiepNguyenex <nguyenvuhiep401@gmail.com>
2026-05-15 15:30:21 +0700

    Buoi 2: EF Core Migration - tao HiepCMS_DB

---
+++ CMS.Backend/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿//sinh vien: nguyen vu hiep
+//mssv:2123110161
+//ngay tao:15/5/26
+
+using Microsoft.AspNetCore.Mvc;
+using CMS.Data.Entities;
+
+namespace CMS.Backend.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var list = new List<Category>
+            {
+                new Category { Id = 1, Name = "Tin Công Nghệ", Description = "Review Laptop, AI" },
+                new Category { Id = 2, Name = "Giáo Dục", Description = "Thông tin tuyển sinh" }
+            };
+            return View(list);
+        }
+    }
+}
